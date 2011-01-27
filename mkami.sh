d9c2f720f3cc500a40e0bff6ccecdaa0b2d91ced
Matthew Emes <memes@matthewemes.com>
2011-01-27 12:18:52 -0800

    Updated files; basic image creation appears sound

---
+++ mkami.sh
@@ -0,0 +1,146 @@
+#! /bin/sh
+#
+# Create a custom AMI
+#
+KVM_SKIP_LAUNCH=${KVM_SKIP_LAUNCH:-""}
+AMI_SKIP=${AMI_SKIP:-""}
+AMI_SKIP_UPLOAD=${AMI_SKIP_UPLOAD:-""}
+DEFAULT_AMI_BUCKET=${AMI_BUCKET:-"matthewemes.com/ami"}
+DEFAULT_DISTRO=${DEFAULT_DISTRO:-debian}
+DEFAULT_ARCH=${DEFAULT_ARCH:-$(arch)}
+DEFAULT_CUSTOM=${DEFAULT_CUSTOM:-""}
+DEFAULT_IMG_SIZE=${DEFAULT_IMG_SIZE:-$((1024 * 1024 * 1024))}
+
+# Use a random password for memes account; public key SSH only
+MEMES_PASSWORD=${MEMES_PASSWORD:-$(dd if=/dev/urandom bs=1 count=8 | base64)}
+
+# Use a random password for root; can be overridden by ENV or custom files
+# remote access via public key SSH only
+ROOT_PASSWORD=${ROOT_PASSWORD:-$(dd if=/dev/urandom bs=1 count=16 2>/dev/null | base64 | tr -cd '[:alnum:]')}
+
+# Script version will hold the information on the creation environment
+script_info="$(basename $0)"
+
+# Source the common functions
+SCRIPT_DIR=${SCRIPT_DIR:-$(dirname $0)}
+[ -r "${SCRIPT_DIR}/ami_functions.sh" ] && . ${SCRIPT_DIR}/ami_functions.sh
+type add_memes_keys >/dev/null 2>/dev/null
+if [ $? -ne 0 ]; then
+    echo "$0: Error: required functions in ami_functions are not available" >&2
+    exit 1
+fi
+
+# After execution remount /home without dev support and unbind shared systems
+trap cleanup 0 1 2 3 15
+
+# Print usage information
+usage()
+{
+    echo "usage: $0 [-d|--distro distro] [-a|--arch arch] [-c|--customise custom]"
+    echo "       where options are:-"
+    echo "        -d distro, --distro distro  distribution to install"
+    echo "           debian or centos, defaults to debian"
+    echo "        -a arch, --arch arch  architecture of AMI"
+    echo "           i386 or x86_64, defaults to architecture of current host"
+    echo "        -c custom, --customise custom  include customisations in "
+    echo "           custom file"
+    echo
+    exit 1
+}
+
+# Simple argument processor
+getargs()
+{
+    local arg=
+    [ $# -eq 0 ] && usage
+    while [ -n "$1" ]; do
+	case "$1" in
+	    -d|--distro)
+		shift
+		distro="$1"
+		[ "debian" = "${distro}" -o "centos" = "${distro}" ] || usage
+		;;
+	    -a|--arch)
+		shift
+		AMI_ARCH="$1"
+		[ "i386" = "${AMI_ARCH}" -o "x86_64" = "${AMI_ARCH}" ] || usage
+		;;
+	    -c|--custom)
+		shift
+		custom="$1"
+		[ -n "${custom}" ] || usage
+		;;
+	    *)
+		usage;
+		exit 1;
+	esac
+	shift
+    done
+}
+
+# Handle script arguments and set to defaults if necessary
+[ $# -ne 0 ] && getargs $@
+[ -z "${distro}" ] && distro="${DEFAULT_DISTRO}"
+[ -z "${AMI_ARCH}" ] && AMI_ARCH="${DEFAULT_ARCH}"
+
+# Make sure environment is correct and ROOT_PKGS are specified
+[ -n "${custom}" ] && prebuild_customise "${SCRIPT_DIR}" "${custom}"
+[ -n "${distro}" ] && prebuild_distro "${SCRIPT_DIR}" "${distro}"
+prebuild_validate
+ec2_validate
+
+base_dir=$(get_base_directory)
+[ -z "${base_dir}" ] && error "need a valid base directory to continue"
+mount_point=$(get_img_mount_point)
+[ -z "${mount_point}" ] && error "need a valid mount point to continue"
+
+# Prepare $base_dir for installation
+rm_chroot "${base_dir}"
+mkdir -p "${base_dir}" || error "unable to create directory ${base_dir}"
+prepare_base "${base_dir}"
+
+# Install the software in a chroot
+prepare_chroot "${base_dir}"
+post_chroot "${base_dir}"
+
+# Handle common configuration scenarios
+update_fstab "${base_dir}"
+update_sshd "${base_dir}"
+add_memes_keys "${base_dir}"
+
+# Finalise any installation action
+post_base "${base_dir}"
+
+# Prepare a KVM instance for testing
+kvm_img=$(get_kvm_img_name)
+kvm_img_size=$(get_kvm_img_size)
+mk_disk_image "${kvm_img}" ${kvm_img_size}
+mount_img "${mount_point}"
+pre_kvm_image "${mount_point}"
+${SUDO} rsync -avP "${base_dir}/" "${mount_point}/" || \
+    error "rsync returned error code $? during copy of files to KVM image"
+post_kvm_image "${mount_point}"
+mk_bootable "${mount_point}"
+umount_img "${mount_point}"
+
+[ -z "${KVM_SKIP_LAUNCH}" ] && launch_img "${kvm_img}"
+
+[ -n "${AMI_SKIP}" ] && exit
+
+# Create an AMI image
+ami_img=$(get_ami_img_name)
+ami_img_size=$(get_ami_img_size)
+mk_fs_image "${ami_img}" ${ami_img_size}
+mount_img "${mount_point}"
+pre_ami_image "${mount_point}"
+${SUDO} rsync -avP "${base_dir}/" "${mount_point}/" || \
+    error "rsync returned error code $? during copy of files to AMI image"
+pre_ami_image "${mount_point}"
+umount_img "${mount_point}"
+bundle_ami "${ami_img}"
+ami_name=$(get_ami_name)
+ami_description=$(get_ami_description)
+ami_bucket=$(get_ami_bucket)
+[ -z "${AMI_SKIP_UPLOAD}" ] && \
+    upload_ami $(basename "${ami_img}") "${ami_bucket}" "${ami_name}" \
+    "${ami_description}"
---
+++ mkdebianami.sh
@@ -0,0 +1,208 @@
+#! /bin/sh
+#
+# Simple shell script to create a Debian image suitable for EC2
+# deployment
+#
+# $Id: $
+
+DEBIAN_VER=${DEBIAN_VER:-stable}
+MIRROR_URL=${MIRROR_URL:-"http://ftp.debian.org"}
+WORKINGDIR=${WORKINGDIR:-"$(pwd)"}
+MOUNTDIR=${MOUNTDIR:-"$(pwd)/debian_${DEBIAN_VER}_${DEBIAN_ARCH}.mnt"}
+ROOT_PKGS=${ROOT_PKGS:-"locales less bzip2"}
+BASE_PKGS=${BASE_PKGS:-"vim sudo openssh-server git subversion mercurial s3cmd"}
+EXTRA_PKGS=${EXTRA_PKGS:-""}
+
+[ "x86_64" = "${AMI_ARCH}" ] && DEBIAN_ARCH="amd64"
+[ -z "${DEBIAN_ARCH}" ] && DEBIAN_ARCH="${AMI_ARCH}"
+
+# Return the directory to use for base install
+distro_get_base_directory()
+{
+    echo "${WORKINGDIR}/centos_${DEBIAN_VER}_${DEBIAN_ARCH}"
+}
+
+# Return the mount point to use for images
+distro_get_img_mount_point()
+{
+    echo "${MOUNTDIR}"
+}
+
+# Install minimum set of packages in order to get functional system
+distro_prepare_base()
+{
+    local base=
+    [ $# -ge 1 ] && base="$1"
+    [ -z "${base}" ] && error "distro_prepare_base: \$base is unspecified"
+    [ -d "${base}" ] || error "distro_prepare_base: ${base} is invalid"
+    ${SUDO} debootstrap --arch ${DEBIAN_ARCH} \
+        ${ROOT_PKGS:+"--include=$(echo ${ROOT_PKGS} | tr ' ' ',')"} \
+        ${DEBIAN_VER} ${base} || \
+        error "distro_prepare_base: debootstrap failed: error code $?"
+}
+
+# Populate the chroot; /proc, /sys, etc are already mounted
+distro_prepare_chroot()
+{
+    local base=
+    [ $# -ge 1 ] && base="$1"
+    [ -z "${base}" ] && error "distro_prepare_chroot: \$base is unspecified"
+    [ -d "${base}" ] || error "distro_prepare_chroot: ${base} is invalid"
+    ${SUDO} chroot "${base}" <<EOF
+# Recreate required devices
+/sbin/MAKEDEV -d mem null zero ram tty console random urandom
+
+# Disable daemon startup during chroot population
+echo "exit 101" > /usr/sbin/policy-rc.d
+chmod 0755 /usr/sbin/policy-rc.d
+echo "America/Los_Angeles" > /etc/timezone
+
+LC_ALL=C debconf-set-selections <<eof
+tzdata  tzdata/Zones/America select Los_Angeles
+locales locales/default_environment_locale select en_US.UTF-8
+locales locales/locales_to_be_generated multiselect en_US.UTF-8 UTF-8
+eof
+LC_ALL=C dpkg-reconfigure --frontend noninteractive --all
+echo "en_US.UTF-8 UTF-8" > /etc/locale.gen
+LC_ALL=C locale-gen
+
+# Add security to the APT sources 
+cat >> /etc/apt/sources.list <<eof
+deb http://security.debian.org stable/updates main
+eof
+apt-get update
+
+# Disable TLS when running under xen
+cat >> /etc/ld.so.conf.d/xen.conf <<eof
+# Disable TLS when the image is running under xen
+hwcap 0 nosegneg
+eof
+ldconfig
+
+# Set the timezone for the instance shells
+cat >> /etc/profile.d/timezone.sh <<eof
+TZ=America/Los_Angeles
+export TZ
+eof
+cat >> /etc/profile.d/timezone.csh <<eof
+setenv TZ America/Los_Angeles
+eof
+
+# Enable networking
+echo "localhost" > /etc/hostname
+cat > /etc/sysctl.d/no_ip6.conf <<eof
+# Disable IPv6
+net.ipv6.conf.all.disable_ipv6=1
+eof
+cat > /etc/network/interfaces <<eof
+auto lo
+iface lo inet loopback
+
+auto eth0
+iface eth0 inet dhcp
+eof
+cat > /etc/hosts <<eof
+127.0.0.1       localhost localhost.localdomain
+eof
+
+# Update sudoers file
+cat >> /etc/sudoers <<eof
+# memes password is random, so don't prompt for sudo
+memes ALL=(ALL) NOPASSWD:ALL
+eof
+
+# Install any remaining base packages or extra packages
+[ -n "${BASE_PKGS}" -o "${EXTRA_PKGS}" ] && \
+    apt-get install -y ${BASE_PKGS} ${EXTRA_PKGS}
+
+# Change root password to consistent value across instances, but
+# remote login is permitted only by public SSH key
+mkdir -p /root/.ssh
+touch /root/.ssh/authorized_keys2
+chmod 0700 /root/.ssh
+chmod 0600 /root/.ssh/authorized_keys2
+chown -R root:root /root/.ssh
+
+# Add memes with random password
+useradd -c "Matthew Emes" -m memes
+mkdir -p /home/memes/.ssh
+touch /home/memes/.ssh/authorized_keys2
+chmod 0700 /home/memes/.ssh
+chmod 0600 /home/memes/.ssh/authorized_keys2
+chown -R memes:memes /home/memes/.ssh
+chpasswd <<eof
+root:${ROOT_PASSWORD}
+memes:${MEMES_PASSWORD}
+eof
+
+# Clean up apt database
+apt-get clean
+EOF
+}
+
+# Clean up after installation
+distro_post_base()
+{
+    local base=
+    [ $# -ge 1 ] && base="$1"
+    [ -z "${base}" ] && error "distro_post_base: \$base is unspecified"
+    [ -d "${base}" ] || error "distro_post_base: ${base} is invalid"
+    ${SUDO} rm -f "${base}/root/.*history" "${base}/usr/sbin/policy-rc.d" || \
+        warn "distro_post_base: error code $? returned while deleting files"
+}
+
+# Return a filename for the AMI image
+distro_get_ami_img_name()
+{
+    echo "${WORKINGDIR}/debian_${DEBIAN_VER}_${DEBIAN_ARCH}.ami.img"
+}
+
+# Return a name for the AMI
+distro_get_ami_name()
+{
+    echo "debian_${DEBIAN_VER}_${DEBIAN_ARCH}.$(date +%Y%m%d)"
+}
+
+# Return a description for the AMI
+distro_get_ami_description()
+{
+    echo "Debian ${DEBIAN_VER} ${DEBIAN_ARCH} image build of $(date '+%H:%M %Z %m/%d/%y')"
+}
+
+# Return a filename for the KVM image
+distro_get_kvm_img_name()
+{
+    echo "${WORKINGDIR}/debian_${DEBIAN_VER}_${DEBIAN_ARCH}.kvm.img"
+}
+
+# Prepare the kvm filesystem image by installing a kernel
+distro_post_kvm_image()
+{
+    local base=
+    [ $# -ge 1 ] && base="$1"
+    [ -z "${base}" ] && error "distro_post_kvm_image: \$base is unspecified"
+    [ -d "${base}" ] || error "distro_post_kvm_image: ${base} is invalid"
+    ${SUDO} mount --bind /proc "${base}/proc" || \
+        error "distro_post_kvm_image: unable to rebind /proc to ${base}/proc"
+    ${SUDO} mount --bind /sys "${base}/sys" || \
+        error "distro_post_kvm_image: unable to rebind /sys to ${base}/sys"
+    local kernel_flavour=
+    [ "i386" = "${DEBIAN_ARCH}" ] && kernel_flavour=686
+    [ -z "${kernel_flavour}" ] && kernel_flavour=${DEBIAN_ARCH}
+    ${SUDO} chroot "${base}" <<EOF
+cat > /etc/kernel-img.conf <<eof
+do_symlinks = no
+relative_links = yes
+do_bootloader = no
+do_bootfloppy = no
+do_initrd = yes
+link_in_boot = no
+eof
+apt-get install -y linux-image-${kernel_flavour}
+rm -f /etc/udev/rules.d/70-persistent-net.rules
+EOF
+    ${SUDO} umount "${base}/sys" || \
+        error "distro_post_kvm_image: unable to umount ${base}/sys"
+    ${SUDO} umount "${base}/proc" || \
+        error "distro_post_kvm_image: unable to umount ${base}/proc"
+}
